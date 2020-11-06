475d693a0075d7a76e9f8a816881414ef6dd4092
Kryoft <fulmine.cris@gmail.com>
2020-11-06 22:04:55 +0100

    Fixes + minor changes

--- LuckyLoot/src/main/java/com/github/kryoft/LuckyLoot.java
+++ LuckyLoot/src/main/java/com/github/kryoft/LuckyLoot.java
@@ -1,10 +1,11 @@
 package com.github.kryoft;
 
 import java.util.Random;
-
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
 import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.loot.ConstantLootTableRange;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.util.registry.Registry;
@@ -12,24 +13,36 @@ import net.minecraft.util.registry.Registry;
 public class LuckyLoot implements ModInitializer {
 	
 	Random random = new Random();
+	Block randomBlock;
+	Item randomItem;
 
 	@Override
 	public void onInitialize() {
 		
 		LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) -> {
 			if (Math.random() > .5F) {
+				randomBlock = Registry.BLOCK.getRandom(random);
+				
 		    	FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
 		    			.rolls(ConstantLootTableRange.create(1))
-		    			.with(ItemEntry.builder(Registry.BLOCK.getRandom(random)));
+		    			.with(ItemEntry.builder(randomBlock));
 		    	
 		    	supplier.pool(poolBuilder);
+		    	
+//		    	System.out.println("id: " + id + "   --->   " + "Random block: " + randomBlock);
 			} else {
+				randomItem = Registry.ITEM.getRandom(random);
+				
 				FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
     			.rolls(ConstantLootTableRange.create(1))
-    			.with(ItemEntry.builder(Registry.ITEM.getRandom(random)));
+    			.with(ItemEntry.builder(randomItem));
     	
 				supplier.pool(poolBuilder);
-			}});
+				
+//				System.out.println("id: " + id + "   --->   " + "Random item: " + randomItem);
+			}
+			}
+		);
 		
 	}
 

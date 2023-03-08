22bb5d2c53a5401b67eaf8874dbae2ab2f9c912e
Kryoft <fulmine.cris@gmail.com>
2023-03-08 23:46:08 +0100

    Updated to 1.19

--- LuckyLoot/src/main/java/com/github/kryoft/LuckyLoot.java
+++ LuckyLoot/src/main/java/com/github/kryoft/LuckyLoot.java
@@ -1,16 +1,17 @@
 package com.github.kryoft;
 
 import java.util.Random;
+
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
-import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 // import net.minecraft.loot.ConstantLootTableRange;  // this doesn't exist anymore in 1.17+ [http://5.9.10.113/66460988/constantloottablerange-fabric-21w08b-is-missing]
 // instead of ConstantLootTableRange, I use ConstantLootNumberProvider
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.LootPool;
 import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.registry.Registries;
 
 public class LuckyLoot implements ModInitializer {
 	
@@ -21,25 +22,31 @@ public class LuckyLoot implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		
-		LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) -> {
+		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
 			if (Math.random() > .5F) {
-				randomBlock = Registry.BLOCK.getRandom(random);
+				randomBlock = Registries.BLOCK.getOrThrow(random.nextInt(Registries.BLOCK.size()));
 				
-		    	FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
-		    			.rolls(ConstantLootNumberProvider.create(1))
-		    			.with(ItemEntry.builder(randomBlock));
+//		    	FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+//		    			.rolls(ConstantLootNumberProvider.create(1))
+//		    			.with(ItemEntry.builder(randomBlock));
+				LootPool.Builder poolBuilder = LootPool.builder()
+						.rolls(ConstantLootNumberProvider.create(1))
+						.with(ItemEntry.builder(randomBlock));
 		    	
-		    	supplier.pool(poolBuilder);
+		    	tableBuilder.pool(poolBuilder.build());
 		    	
 //		    	System.out.println("id: " + id + "   --->   " + "Random block: " + randomBlock);
 			} else {
-				randomItem = Registry.ITEM.getRandom(random);
+				randomItem = Registries.ITEM.getOrThrow(random.nextInt(Registries.ITEM.size()));
 				
-				FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
-    			.rolls(ConstantLootNumberProvider.create(1))
-    			.with(ItemEntry.builder(randomItem));
+//				FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+//    			.rolls(ConstantLootNumberProvider.create(1))
+//    			.with(ItemEntry.builder(randomItem));
+				LootPool.Builder poolBuilder = LootPool.builder()
+						.rolls(ConstantLootNumberProvider.create(1))
+						.with(ItemEntry.builder(randomItem));
     	
-				supplier.pool(poolBuilder);
+				tableBuilder.pool(poolBuilder);
 				
 //				System.out.println("id: " + id + "   --->   " + "Random item: " + randomItem);
 			}
